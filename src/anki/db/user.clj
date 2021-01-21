9a07fcf8bbb522d90a70ef588bf999b472031a99
believelody <believelody@gmail.com>
2021-01-21 07:17:46 +0100

    added generation to spec to generate example data

--- src/anki/db/user.clj
+++ src/anki/db/user.clj
@@ -1,7 +1,8 @@
 (ns anki.db.user
   (:require [datomic.api :as d]
             [clojure.string :as str]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.test.check.generators :as gen]))
 
 (defn validate-email [email]
   (let [email-regex #"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"]
@@ -9,11 +10,25 @@
 
 (validate-email "joe@gmail.com")
 
-(s/def :user/email (s/and string? validate-email))
-(s/def :user/password (s/and string? #(> (count %) 3)))
-(s/def :user/username string?)
-(s/def :user/token string?)
+(s/def :user/email (s/with-gen
+                     (s/and string? validate-email)
+                     #(s/gen #{"t1@t.com" "t2@t.com"})))
+(s/def :user/password (s/with-gen
+                        (s/and string? #(> (count %) 3))
+                        #(s/gen #{"azerty" "1234" "abc"})))
+(s/def :user/username (s/with-gen
+                        string?
+                        #(s/gen #{"t1" "t2" "t3"})))
+(s/def :user/token (s/with-gen
+                     string?
+                     #(s/gen #{"token1" "token2" "token3"})))
 (s/def :user/id string?)
+
+(gen/generate (s/gen :user/email))
+(gen/generate (s/gen :user/password))
+(gen/generate (s/gen :user/username))
+(gen/generate (s/gen :user/token))
+(gen/generate (s/gen ::user))
 
 ;; (s/valid? :user/email "madamada")
 ;; (s/explain :user/email "madamada")
